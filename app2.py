8052fd62eb3f10012a729c3f2b8e6c368b8b2b88
xxxxxikke <161652540+xxxxxikke@users.noreply.github.com>
2025-06-15 23:02:57 +0200

    cors

--- app2.py
+++ app2.py
@@ -10,8 +10,11 @@ from wtforms import PasswordField, StringField, Form
 from wtforms.validators import DataRequired, Email
 from wtforms.widgets import TextInput
 
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "https://clearbuypicks.onrender.com"}})
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mijngeheim123')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
