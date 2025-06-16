4d5f1d610644bbea4a529d736945446476a471e3
xxxxxikke <161652540+xxxxxikke@users.noreply.github.com>
2025-06-16 07:23:16 +0200

    database

--- app2.py
+++ app2.py
@@ -41,9 +41,7 @@ serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 CORS(app, resources={r"/api/*": {"origins": "https://clearbuypicks.onrender.com"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL',
-    'sqlite:///data.db'  # fallback voor lokaal testen
-)
+    'DATABASE_URL')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -197,3 +195,5 @@ def reset_password():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
+
+
